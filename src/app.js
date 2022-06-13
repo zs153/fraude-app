5c5e606f7916aa6ee053fa930f928fc5ff01f8fe
zs153 <tmp153@gmail.com>
2022-06-13 13:36:32 +0200

    upd notificaciones

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ const app = express();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
+// middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/bin/www.js
+++ src/bin/www.js
@@ -7,7 +7,7 @@ import http from "http";
 import app from "../app";
 import { Server } from "socket.io";
 
-let notes = [{ senderId: 'SYSTEM', message: 'Bienvenido a Gestión de Fraude SIAC', fecha: new Date().toLocaleString('fr-FR') }];
+let notes = [];
 let users = [];
 
 /**
@@ -75,26 +75,24 @@ const onListening = () => {
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  //console.log("user connected", socket.id);
-  socket.on('client:addUser', (userID) => {
-    addUser(userID, socket.id)
-    //io.emit('server:users', users)
-    io.to(socket.id).emit('server:loadNotes', notes)
-  });
+  //io.to(socket.id).emit('server:loadNotes', notes)
+  // socket.on('client:addUser', (userID) => {
+  //   addUser(userID, socket.id)
+  //   io.emit('server:users', users)
+  // });
   socket.on("client:newNote", (note) => {
     addNote(note)
-    io.emit("server:newNote", note)
+    io.emit('server:newNote', note)
   });
-  socket.on("client:newNoteTo", (note) => {
-    const user = getUser(note.receiverId)
-    addNote(note)
-    io.to(user.socketId).emit("server:newNote", note)
-  });
-  socket.on("disconnect", () => {
-    delUser(socket.id)
-    //io.emit('server:users', users)
-    console.log("Desconectado");
-  });
+  // socket.on("client:newNoteTo", (note) => {
+  //   const user = getUser(note.receiverId)
+  //   addNote(note)
+  //   io.to(user.socketId).emit("server:newNote", note)
+  // });
+  // socket.on("disconnect", () => {
+  //   delUser(socket.id)
+  //   io.emit('server:users', users)
+  // });
 });
 
 /**
