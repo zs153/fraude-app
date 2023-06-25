b268a8854cf1196078e0ff5a2b7c0e56c5ced40f
zs53 <tmp153@gmail.com>
2023-06-25 20:14:03 +0200

    upd

--- src/routes/gente.router.js
+++ src/routes/gente.router.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { getGente } from "../controllers/gente.controller";
 
-const apiSmsRouter = express.Router();
+const apiGenteRouter = express.Router();
 
 // smss
-apiSmsRouter.post("/gente", getGente);
+apiGenteSmsRouter.post("/gente", getGente);
 
-export default apiSmsRouter;
+export default apiGenteRouter;
