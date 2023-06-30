5a49113a7d79b1816dc512518fbfe66659ab15d0
zs53 <tmp153@gmail.com>
2023-06-30 11:44:51 +0200

    upd

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,72 +35,75 @@ module.exports = function (grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       js: {
-        // files: {
-        //   // bin
-        //   'dist/bin/www.js': ['./dist/bin/www.js'],
-        //   // config
-        //   'dist/config/settings.js': ['./dist/config/settings.js'],
-        //   // controllers
-        //   'dist/controllers/admin/carga.controller.js':     ['./dist/controllers/admin/carga.controller.js'],
-        //   'dist/controllers/admin/estadistica.controller.js':   ['./dist/controllers/admin/estadistica.controller.js'],
-        //   'dist/controllers/admin/fraude.controller.js': ['./dist/controllers/admin/fraude.controller.js'],
-        //   'dist/controllers/admin/historico.controller.js':   ['./dist/controllers/admin/historico.controller.js'],
-        //   'dist/controllers/admin/oficina.controller.js':  ['./dist/controllers/admin/oficina.controller.js'],
-        //   'dist/controllers/admin/tipos.controller.js':  ['./dist/controllers/admin/tipos.controller.js'],
-        //   'dist/controllers/admin/usuario.controller.js':   ['./dist/controllers/admin/usuario.controller.js'],
-        //   //
-        //   'dist/controllers/user/fraude.controller.js':['./dist/controllers/user/fraude.controller.js'],
-        //   'dist/controllers/user/usuario.controller.js':    ['./dist/controllers/user/usuario.controller.js'],
-        //   //
-        //   'dist/controllers/main.controller.js': ['./dist/controllers/main.controller.js'],
-        //   // middleware
-        //   'dist/middleware/auth.js': ['./dist/middleware/auth.js'],
-        //   // public-js
-        //   'dist/public/js/addCarga.min.js': ['./dist/public/js/addCarga.min.js'],
-        //   'dist/public/js/addEditEvento.min.js': ['./dist/public/js/addEditEvento.min.js'],
-        //   'dist/public/js/addEditFraude.min.js': ['./dist/public/js/addEditFraude.min.js'],
-        //   'dist/public/js/addEditOficina.min.js': ['./dist/public/js/addEditOficina.min.js'],
-        //   'dist/public/js/addEditRelacion.min.js': ['./dist/public/js/addEditRelacion.min.js'],
-        //   'dist/public/js/addEditSms.min.js': ['./dist/public/js/addEditSms.min.js'],
-        //   'dist/public/js/addEditTipoCierre.min.js': ['./dist/public/js/addEditTipoCierre.min.js'],
-        //   'dist/public/js/addEditTipoEvento.min.js': ['./dist/public/js/addEditTipoEvento.min.js'],
-        //   'dist/public/js/addEditTipoFraude.min.js': ['./dist/public/js/addEditTipoFraude.min.js'],
-        //   'dist/public/js/addEditTipoHito.min.js': ['./dist/public/js/addEditTipoHito.min.js'],
-        //   'dist/public/js/addEditUsuario.min.js': ['./dist/public/js/addEditUsuario.min.js'],
-        //   'dist/public/js/addEjercicio.min.js': ['./dist/public/js/addEjercicio.min.js'],
-        //   'dist/public/js/addHito.min.js': ['./dist/public/js/addHito.min.js'],
-        //   'dist/public/js/asignar.min.js': ['./dist/public/js/asignar.min.js'],
-        //   'dist/public/js/ayuda.min.js': ['./dist/public/js/ayuda.min.js'],
-        //   'dist/public/js/desasignar.min.js': ['./dist/public/js/desasignar.min.js'],
-        //   'dist/public/js/editHistorico.min.js': ['./dist/public/js/editHistorico.min.js'],
-        //   'dist/public/js/editHito.min.js': ['./dist/public/js/editHito.min.js'],
-        //   'dist/public/js/enumeraciones.js': ['./dist/public/js/enumeraciones.js'],
-        //   'dist/public/js/indexAdes.min.js': ['./dist/public/js/indexAdes.min.js'],
-        //   'dist/public/js/indexCargas.min.js': ['./dist/public/js/indexCargas.min.js'],
-        //   'dist/public/js/indexFraudes.min.js': ['./dist/public/js/indexFraudes.min.js'],
-        //   'dist/public/js/indexFraudesAdm.min.js': ['./dist/public/js/indexFraudesAdm.min.js'],
-        //   'dist/public/js/indexHistoricos.min.js': ['./dist/public/js/indexHistoricos.min.js'],
-        //   'dist/public/js/indexOficinas.min.js': ['./dist/public/js/indexOficinas.min.js'],
-        //   'dist/public/js/indexRelaciones.min.js': ['./dist/public/js/indexRelaciones.min.js'],
-        //   'dist/public/js/indexResueltos.min.js': ['./dist/public/js/indexResueltos.min.js'],
-        //   'dist/public/js/indexResueltosAdm.min.js': ['./dist/public/js/indexResueltosAdm.min.js'],
-        //   'dist/public/js/indexSmss.min.js': ['./dist/public/js/indexSmss.min.js'],
-        //   'dist/public/js/indexStats.min.js': ['./dist/public/js/indexStats.min.js'],
-        //   'dist/public/js/indexTiposCierre.min.js': ['./dist/public/js/indexTiposCierre.min.js'],
-        //   'dist/public/js/indexTiposEvento.min.js': ['./dist/public/js/indexTiposEvento.min.js'],
-        //   'dist/public/js/indexTiposFraude.min.js': ['./dist/public/js/indexTiposFraude.min.js'],
-        //   'dist/public/js/indexTiposHito.min.js': ['./dist/public/js/indexTiposHito.min.js'],
-        //   'dist/public/js/indexUsuarios.min.js': ['./dist/public/js/indexUsuarios.min.js'],
-        //   'dist/public/js/perfil.min.js': ['./dist/public/js/perfil.min.js'],
-        //   // routes
-        //   'dist/routes/admin.router.js': ['./dist/routes/admin.router.js'],
-        //   'dist/routes/main.router.js': ['./dist/routes/main.router.js'],
-        //   'dist/routes/user.router.js':['./dist/routes/user.router.js'],
-        //   // app
-        //   'dist/app.js': ['./dist/app.js'],
-        // }
-        src: ['./src/views/admin/fraudes/resueltos/indexResueltos.js'],
-        dest: './src/public/js/admin/indexResueltos.min.js'
+        files: {
+          // bin
+          'dist/bin/www.js': ['./dist/bin/www.js'],
+          // config
+          'dist/config/settings.js': ['./dist/config/settings.js'],
+          // controllers
+          // admin
+          'dist/controllers/admin/carga.controller.js':     ['./dist/controllers/admin/carga.controller.js'],
+          'dist/controllers/admin/estadistica.controller.js':   ['./dist/controllers/admin/estadistica.controller.js'],
+          'dist/controllers/admin/fraude.controller.js': ['./dist/controllers/admin/fraude.controller.js'],
+          'dist/controllers/admin/historico.controller.js':   ['./dist/controllers/admin/historico.controller.js'],
+          'dist/controllers/admin/oficina.controller.js':  ['./dist/controllers/admin/oficina.controller.js'],
+          'dist/controllers/admin/tipos.controller.js':  ['./dist/controllers/admin/tipos.controller.js'],
+          'dist/controllers/admin/usuario.controller.js':   ['./dist/controllers/admin/usuario.controller.js'],
+          // user
+          'dist/controllers/user/fraude.controller.js':['./dist/controllers/user/fraude.controller.js'],
+          'dist/controllers/user/usuario.controller.js':    ['./dist/controllers/user/usuario.controller.js'],
+          // main
+          'dist/controllers/main.controller.js': ['./dist/controllers/main.controller.js'],
+          // middleware
+          'dist/middleware/auth.js': ['./dist/middleware/auth.js'],
+          // public-js
+          'dist/public/js/admin/addCarga.min.js': ['./dist/public/js/admin/addCarga.min.js'],
+          'dist/public/js/admin/addEditFraude.min.js': ['./dist/public/js/admin/addEditFraude.min.js'],
+          'dist/public/js/admin/addEditOficina.min.js': ['./dist/public/js/admin/addEditOficina.min.js'],
+          'dist/public/js/admin/addEditTipoCierre.min.js': ['./dist/public/js/admin/addEditTipoCierre.min.js'],
+          'dist/public/js/admin/addEditTipoEvento.min.js': ['./dist/public/js/admin/addEditTipoEvento.min.js'],
+          'dist/public/js/admin/addEditTipoFraude.min.js': ['./dist/public/js/admin/addEditTipoFraude.min.js'],
+          'dist/public/js/admin/addEditTipoHito.min.js': ['./dist/public/js/admin/addEditTipoHito.min.js'],
+          'dist/public/js/admin/addEditUsuario.min.js': ['./dist/public/js/admin/addEditUsuario.min.js'],
+          'dist/public/js/admin/asignar.min.js': ['./dist/public/js/admin/asignar.min.js'],
+          'dist/public/js/admin/desasignar.min.js': ['./dist/public/js/admin/desasignar.min.js'],
+          'dist/public/js/admin/editHistorico.min.js': ['./dist/public/js/admin/editHistorico.min.js'],
+          'dist/public/js/admin/indexAdes.min.js': ['./dist/public/js/admin/indexAdes.min.js'],
+          'dist/public/js/admin/indexCargas.min.js': ['./dist/public/js/admin/indexCargas.min.js'],
+          'dist/public/js/admin/indexFraudes.min.js': ['./dist/public/js/admin/indexFraudes.min.js'],
+          'dist/public/js/admin/indexHistoricos.min.js': ['./dist/public/js/admin/indexHistoricos.min.js'],
+          'dist/public/js/admin/indexOficinas.min.js': ['./dist/public/js/admin/indexOficinas.min.js'],
+          'dist/public/js/admin/indexResueltos.min.js': ['./dist/public/js/admin/indexResueltos.min.js'],
+          'dist/public/js/admin/indexStats.min.js': ['./dist/public/js/admin/indexStats.min.js'],
+          'dist/public/js/admin/indexTiposCierre.min.js': ['./dist/public/js/admin/indexTiposCierre.min.js'],
+          'dist/public/js/admin/indexTiposEvento.min.js': ['./dist/public/js/admin/indexTiposEvento.min.js'],
+          'dist/public/js/admin/indexTiposFraude.min.js': ['./dist/public/js/admin/indexTiposFraude.min.js'],
+          'dist/public/js/admin/indexTiposHito.min.js': ['./dist/public/js/admin/indexTiposHito.min.js'],
+          'dist/public/js/admin/indexUsuarios.min.js': ['./dist/public/js/admin/indexUsuarios.min.js'],
+          // user-js
+          'dist/public/js/user/addEditEvento.min.js': ['./dist/public/js/user/addEditEvento.min.js'],
+          'dist/public/js/user/addEditFraude.min.js': ['./dist/public/js/user/addEditFraude.min.js'],
+          'dist/public/js/user/addEditSms.min.js': ['./dist/public/js/user/addEditSms.min.js'],
+          'dist/public/js/user/addEjercicio.min.js': ['./dist/public/js/user/addEjercicio.min.js'],
+          'dist/public/js/user/addHito.min.js': ['./dist/public/js/user/addHito.min.js'],
+          'dist/public/js/user/editHito.min.js': ['./dist/public/js/user/editHito.min.js'],
+          'dist/public/js/user/indexFraudes.min.js': ['./dist/public/js/user/indexFraudes.min.js'],
+          'dist/public/js/user/indexRelaciones.min.js': ['./dist/public/js/user/indexRelaciones.min.js'],
+          'dist/public/js/user/indexResueltos.min.js': ['./dist/public/js/user/indexResueltos.min.js'],
+          'dist/public/js/user/indexSmss.min.js': ['./dist/public/js/user/indexSmss.min.js'],
+          'dist/public/js/user/perfil.min.js': ['./dist/public/js/user/perfil.min.js'],
+          // 
+          'dist/public/js/ayuda.min.js': ['./dist/public/js/ayuda.min.js'],
+          'dist/public/js/enumeraciones.js': ['./dist/public/js/enumeraciones.js'],
+          // routes
+          'dist/routes/admin.router.js': ['./dist/routes/admin.router.js'],
+          'dist/routes/main.router.js': ['./dist/routes/main.router.js'],
+          'dist/routes/user.router.js':['./dist/routes/user.router.js'],
+          // app
+          'dist/app.js': ['./dist/app.js'],
+        }
+        // src: ['./src/views/admin/fraudes/resueltos/indexResueltos.js'],
+        // dest: './src/public/js/admin/indexResueltos.min.js'
       },
     },
   });
